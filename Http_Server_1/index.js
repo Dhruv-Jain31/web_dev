ca066b3805f1bd7e8c68418c392a8cb530a21705
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-03-17 02:34:43 +0530

    first http server created

---
+++ Http_Server_1/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express() // initializes instance of express
+const port = 3000
+
+/*similar to fs.readfile() which has the path of the file
+utf-7 and a function which is called when it reads the file
+similarly this app.get will be call the function whenever
+someone hits the backend server*/
+
+app.get('/', function(req, res)  {
+  res.send('Hello World!')
+})
+
+app.listen(port)
