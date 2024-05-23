68a9316ec2be4c865cbf81881d1e987707f66fec
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-05-24 00:05:17 +0530

    made a custom map function

---
+++ js_advanced/map.js
@@ -0,0 +1,25 @@
+//create a map function that takes 2 inputs an array and a transformation function and 
+//transforms the array into a new one using the transformation function in javascript.
+
+function own_map(array,transform){
+
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+
+        const transformed_element = transform(array[i]);
+
+        result.push(transformed_element)
+    }
+
+    return result
+}
+
+//eg
+const number = [1,2,3,4,5,6,7,]
+
+function square(i){
+    return i*i
+}
+
+const squared_numbers = own_map(number, square)
+console.log(squared_numbers);
