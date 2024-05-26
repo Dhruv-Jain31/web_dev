ca1cd29138fbd55e11ad5813f4fd30d88aac16d2
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-05-26 17:43:58 +0530

    added middleware to check number of requests coming on backend

--- Error_handling/user_auth_mw.js
+++ Error_handling/user_auth_mw.js
@@ -2,6 +2,15 @@ const express = require("express")
 
 const app = express()
 
+const calculate_requests = 0
+
+function number_of_requests(req,res,next) {
+    calculate_requests += 1
+    console.log("no.of requests on server are",calculate_requests)
+}
+
+app.use(number_of_requests)
+
 function userMiddleware(req, res, next) {
     const username = req.headers.username;
     const password = req.headers.password;
@@ -20,5 +29,40 @@ function kidneyMiddleware(req, res, next) {
     if (kidneyId != 1 && kidneyId !=2) {
         res.status(0).json({"msg" : "invalid inputs"})
     }
+    else{
+        next()
+    }
 }
+
+function heartMiddleware(req,res,next){
+    const heart_status = req.query.heart_status;
+
+    if (heart_status == "healthy"){
+        res.status(200).json({"msg" : "healthy heart"})
+    }
+    else{
+        res.status(400).json({"msg" : "unhealthy heart"})
+        next()
+    }
+}
+
+app.get("/health_checkup", userMiddleware,function(req,res){
+
+    // requied logic
+
+    res.status(200).send("done. proceed furhter")
+})
+
+app.get("/kidney_checkup",userMiddleware,kidneyMiddleware,function(req,res){
+
+    // required logic
+
+    res.status(200).send("kidney checkup successfull")
+})
+
+app.get("/heart_checkup",userMiddleware,heartMiddleware,function(req,res){
+    // req logic
+
+    res.status(200).send("heart checkup successfull")
+})
 
