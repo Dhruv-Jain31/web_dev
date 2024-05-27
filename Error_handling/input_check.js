16d48c0e880f058eb590c8b5489f10aa30b1ff53
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-05-27 22:31:48 +0530

    zod library

--- Error_handling/input_check.js
+++ Error_handling/input_check.js
@@ -1,6 +1,8 @@
 // for input checks primarily zod library is used.
 // schema is the inputs that we enter
-// parsing is perfoming checks on the inputs provided.
+// parsing is performing checks on the inputs provided.
 
 const express = require("express");
 const zod = require("zod")
+const app = express();
+
