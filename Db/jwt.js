48f6e68e28e6971835c4fd5977f2ceb8028a646c
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-08 23:54:42 +0530

    added the username check function

--- Db/jwt.js
+++ Db/jwt.js
@@ -16,9 +16,10 @@ Returns 403 status code if not
 
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const jwtPassword = "123456";
+const jwtPassword = "123456"; // password on the server used for verification
 
 const app = express();
+app.use(express.json());
 
 const ALL_USERS = [
   {
@@ -41,6 +42,14 @@ const ALL_USERS = [
 function userExists(username, password) {
   // write logic to return true or false if this user exists
   // in ALL_USERS array
+
+  let userExists = false;
+  for (let i = 0; i < ALL_USERS.length; i++) {
+    if (ALL_USERS[i].username == username && ALL_USERS[i].password == password) {
+      userExists = true;
+    }
+  }
+  return userExists;
 }
 
 app.post("/signin", function (req, res) {
