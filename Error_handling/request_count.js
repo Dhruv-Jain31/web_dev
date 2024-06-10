d943fa5533723dcafaaeaa81829d34e31993a6d7
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-10 15:59:08 +0530

    added rate_limitter middleware function

--- Error_handling/request_count.js
+++ Error_handling/request_count.js
@@ -15,6 +15,37 @@ app.use(function(req, res, next) {
   next();
 })
 
+// You have been given an express server which has a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// rate limit the requests from a user to only 5 request per second
+// If a user sends more than 5 requests in a single second, the server
+// should block them with a 404.
+// User will be sending in their user id in the header as 'user-id'
+// You have been given a numberOfRequestsForUser object to start off with which
+// clears every one second
+
+app.use(function(req, res, next) {
+    const userId = req.headers["userId"];
+  
+    if (numberOfRequestsForUser[userId]) {  // obejct has one entry with userId as one index and no.of requests as it's value.
+  
+      numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1;
+      if (numberOfRequestsForUser[userId] > 5){
+        res.status(404)
+      }
+  
+      else{
+        next();
+      }
+  
+    }
+    else{
+      numberOfRequestsForUser[userId] = 1;
+      next();
+    }
+
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
