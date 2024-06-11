4c92e383acbe6dc1b698693f253d4e6b2efff67f
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-11 21:32:42 +0530

    added /courses route to create new courses with proper checks

--- Database/routes/admin.js
+++ Database/routes/admin.js
@@ -2,6 +2,9 @@ const express = require("express");
 const adminMiddleware = require("../middleware/admin");
 const { Admin } = require("../Db_schema");
 const router = express.Router();
+const zod = require("zod");
+const { title } = require("process");
+const { createDecipheriv } = require("crypto");
 
 // Admin Routes
 // it does not mean it handles the signup endpoint. it handles /admin/signup end-point.
@@ -37,8 +40,60 @@ router.post('/signup', (req, res) => {
 });
 
 
+function validate_input(obj){
+    const schema = zod.object({
+
+        title: zod.string().nonempty({ message: "Title is required" }),
+        description: zod.string().nonempty({ message: "Description is required" }),
+        imageLink: zod.string().url()({ message: "Invalid URL format" }),
+        price: zod.number().positive({ message: "Price must be a positive number" })
+    })
+
+    const response = schema.safeParse(obj);
+    console.log(response);
+    return response;
+
+}
+
 router.post('/courses', adminMiddleware, (req, res) => {
     // Implement course creation logic
+
+    const input = req.body
+    const response = validate_input(input)
+    if(!response.success){
+        res.status(411).json({
+            "msg": "Invalid Inputs",
+            "errors": response.error.errors
+        });
+    }
+    else{
+
+        Course.create({
+
+            title: title,
+            description: description,
+            imageLink: imageLink,
+            price: price
+
+        })
+
+        .then(function(value){
+
+            res.json({
+                "msg" : "Course created successfully",
+                courseId: value.id  // returns the course id from the db.
+            })
+        })
+
+        .catch(function(err){
+
+            res.status(500).json({
+
+                "msg" : "Course could not be created",
+                "error" : "There is some issue with our server"
+            })
+        })
+    }
 });
 
 router.get('/courses', adminMiddleware, (req, res) => {
