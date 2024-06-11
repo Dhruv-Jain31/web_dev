41d437f60ffa639ad3940068fa8f4974de452097
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-11 16:32:17 +0530

    added all the schemas

--- Database/Db_schema/index.js
+++ Database/Db_schema/index.js
@@ -6,14 +6,26 @@ mongoose.connect('mongodb+srv://dhruv:dhruv@cluster0.9nyvtnq.mongodb.net/');
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'  // means it refers to the course table.
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    Price: Number,
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
