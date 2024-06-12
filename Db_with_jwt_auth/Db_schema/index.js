2fe9c4b155a7675548c3579e96929d87d219c5a2
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-13 00:24:47 +0530

    defined db schema and basic middleware code in admin.js for jwt auth

---
+++ Db_with_jwt_auth/Db_schema/index.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://dhruv:dhruv@cluster0.9nyvtnq.mongodb.net/course_selling_app');
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String,
+});
+
+const UserSchema = new mongoose.Schema({
+    // Schema definition here
+    username: String,
+    password: String,
+
+    /*This field is an array of ObjectIds.
+type: mongoose.Schema.Types.ObjectId indicates that each element in the array is a MongoDB ObjectId.
+ref: 'Course' creates a reference to the Course collection.
+This means each ObjectId in the purchasedCourses array corresponds to a document in the Course collection.*/
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+const CourseSchema = new mongoose.Schema({
+    // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    Price: Number,
+});
+
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin,
+    User,
+    Course
+}
