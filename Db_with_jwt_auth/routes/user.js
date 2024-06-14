7b2c0139ff70dc1ce4c2750fa67935059e351cfa
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-14 14:53:52 +0530

    modified signup route and sign in route to remove redundancy in database

--- Db_with_jwt_auth/routes/user.js
+++ Db_with_jwt_auth/routes/user.js
@@ -30,26 +30,91 @@ router.post('/signup', (req, res) => {
     const username = response.username;
     const password = response.password;
 
-    User.create({
-        username: username,
-        password: password,
+    User.findOne({
+        username:username
     })
-    .then(function(user){
-        res.json({
-            "msg": "user created successfully",
-            "credentials": user
-        })
+    .then(function(value){
+        console.log(value)
+
+        if (value){  // don't use value.username here because
+            // If value is null (i.e., no user found), then accessing value.username will result in an error.. 
+            res.status(411).json({
+                "msg" : "this username already exists"
+            })
+        }
+        else{  // this is put in else condition to prevent race condition.
+            //User.create might execute before User.findOne completes.
+
+            User.create({
+                username: username,
+                password: password,
+            })
+            .then(function(user){
+                res.json({
+                    "msg": "user created successfully",
+                    "credentials": user
+                })
+            })
+            .catch(function(err){
+                res.json({
+                    "msg": "Some error occured in creating user",
+                    "error": err
+                })
+            })
+        }
     })
     .catch(function(err){
-        res.json({
-            "msg": "Some error occured in creating user",
-            "error": err
+        res.status(500).json({
+            "msg": "there is some issue with our servers",
+            "error" : err.message
         })
-    })
-});
+    });
+});    
 
 router.post('/signin', (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({
+        username: username, // imp 
+    })
+    .then(function(user) {
+        console.log(user)
+        if (!user) {
+            // Username is incorrect
+            res.status(500).json({
+                "msg": "Incorrect username"
+            });
+        } 
+        else if (user.password !== password) {
+            // Username is correct, but password is incorrect
+            res.status(500).json({
+                "msg": "Incorrect password"
+            });
+        } 
+        else {
+            // means Username and password are correct
+            const token = jwt.sign({ 
+                username,
+                password
+            }, JWT_SECRET);
+
+            const decoded_token = jwt.decode(token, JWT_SECRET);
+            console.log(decoded_token);
+
+            res.json({
+                "msg": "User sign in successful",
+                "token": token
+            });
+        }
+    })
+    .catch(function(error) {
+        res.status(500).json({
+            msg: "Internal server error",
+            error: error
+        });
+    });
 });
 
 router.get('/courses', (req, res) => {
