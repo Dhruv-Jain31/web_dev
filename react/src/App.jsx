a057b0f8e5901066fe82a136e6ffd2349cd06bd9
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-22 00:30:35 +0530

    wrote basic on click handler

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,22 @@
-// inside a jsx file we can return html from a function
+// inside a jsx file we can return html from a function. but it is slightly different from html
+// jsx = js + xml
 // state
 let state = {
   count: 0
 }
 
 // component. {..} are used to render a state in a component
+//purpose of react is: give it a component, give it a state update the state
+// react will automatically re renders the updated state to the DOM.
 function App(){
+
+  function onClickHandler(){
+    state.count = state.count + 1;
+  }
+
   return (
     <div>
-      <button>Counter {state.count}</button> 
+      <button onClick={onClickHandler}>Counter {state.count}</button> 
     </div>
   )
 }
