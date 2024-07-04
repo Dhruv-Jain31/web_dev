dc260acefab1d5e97e96ace593d49dd251d1a5d8
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-07-04 16:26:47 +0530

    completed minimum re renders

--- react_practice/2.minimize_rerenders/src/App.jsx
+++ react_practice/2.minimize_rerenders/src/App.jsx
@@ -9,6 +9,22 @@ import './App.css'
 // so we push the state down to minimize re rendering
 
 function App() {
+
+  return (
+    // this cannot return multiple siblings together,
+    // it can only return one sibling at a time.
+    // for multiple siblings they must be wrapped in a fragment/parent.
+    <div>
+      <Header_With_Button></Header_With_Button>
+      <Header title = <br></br>>  </Header>
+      <Header title = "Dhruv_jain">  </Header>
+      <Header title = "Dhruv_jain2"> </Header>
+    </div>
+  )
+}
+
+function Header_With_Button(){
+
   const [Title, setTitle] = useState("Dhruv")
   const [Description, setDescription] = useState("Dj_2")
 
@@ -16,19 +32,12 @@ function App() {
     setTitle("my name is " + Math.random());
     setDescription("my name is " + Math.floor(Math.random() * 100));
   }
-  return (
-    // this cannot return multiple siblings together,
-    // it can only return one sibling at a time.
-    // for multiple siblings they must be wrapped in a fragment/parent.
-    <div>
-      <button onClick={updateTitle}>Click me to see random numbers </button>
-      <Header title = {Title}>  </Header>
-      <Header title = {Description}>  </Header>
-      <Header title = <br></br>>  </Header>
-      <Header title = "Dhruv_jain">  </Header>
-      <Header title = "Dhruv_jain2"> </Header>
-    </div>
-  )
+
+  return <div>
+    <button onClick={updateTitle}>Click me to see random numbers </button>
+    <Header title = {Title}>  </Header>
+    <Header title = {Description}>  </Header>
+  </div>
 }
 
 function Header({title}){
