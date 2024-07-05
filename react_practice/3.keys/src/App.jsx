fccc587ff9df1088af7d1f56ca9fdc784c036718
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-07-05 16:18:55 +0530

    added basic code

--- react_practice/3.keys/src/App.jsx
+++ react_practice/3.keys/src/App.jsx
@@ -1,35 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, {Fragement} from 'react'
 import './App.css'
 
+let id = 1;
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+    id: id,
+    title: "go to gym",
+    description: "go to gym today"
+  },{
+    id: id++,
+    title: "do DSA",
+    description: "complete arrays"
+  }],{
+    id: id++,
+    title: "go to class",
+    description: "study micro processor"
+  })
+
+  function addTodo(){
+    setTodos([...todos,{ //spread operator: spreads all the existing and adds the 4th todo
+      id: id++,
+      title: Math.random(),
+      description: Math.random()
+    }])
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <button onClick={addTodo}>Add a todo</button>
+
+      {todos.map(function(todo){
+        return <Todo title = {todo.title} id={id} description = {todo.description} />
+      })}
+
+    </div>
   )
+}
+
+function Todo({title, description}){
+  return <div>
+    <i>
+      {id}
+    </i>
+    <h1>
+      {title}
+    </h1>
+    <h5>
+      {description}
+    </h5>
+  </div>
 }
 
 export default App
