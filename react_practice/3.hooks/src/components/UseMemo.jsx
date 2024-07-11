d6824234dff243c2a04c57eff881f0c17095ad97
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-07-11 23:19:49 +0530

    done use memo using use effect

--- react_practice/3.hooks/src/components/UseMemo.jsx
+++ react_practice/3.hooks/src/components/UseMemo.jsx
@@ -3,30 +3,35 @@
 //2. Lets user put a value in an input box (n) and you need
 //to show sum from 1-n
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Fragment } from "react";
 
 export function UseMemo(){
     const [counter, setCounter] = useState(0);
     const [value, setValue] = useState(1);
+    const [count_value, set_count_value] = useState(0);
 
-    let count = 0;
-    for (let i = 1; i<= value; i++){
-        count = count + i;
-    }
+    useEffect(function(){
+        let count = 0;
+        for (let i = 1; i<= value; i++){
+           count = count + i;
+        }
+        set_count_value(count);
+    },[value])
 
-    return
+    return(
     <Fragment>
         <div>
             <input onChange={function(e){
-                setValue(e.target.value)
+                setValue(e.target.value);
             }} placeholder={"Find sum from 1 to n"}></input>
             <br />
-            Sum from 1 to {value} is {count}
+            Sum from 1 to {value} is {count_value}
             <br />
             <button onClick = {function (){
                 setCounter(counter + 1)
             }}>Counter {counter} </button>
         </div>
     </Fragment>
+    )
 }
