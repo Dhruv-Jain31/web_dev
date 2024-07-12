f9d8a0fa5f3be7d6d82c055a9afdf74fbd18b073
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-07-12 23:43:23 +0530

    added use callback.jsx function

--- react_practice/3.hooks/src/components/UseMemo.jsx
+++ react_practice/3.hooks/src/components/UseMemo.jsx
@@ -3,12 +3,16 @@
 //2. Lets user put a value in an input box (n) and you need
 //to show sum from 1-n
 
-import { useMemo, useState } from "react";
+import { useMemo, useState, useEffect } from "react";
 import { Fragment } from "react";
 
 export function UseMemo(){
     const [counter, setCounter] = useState(0);
     const [value, setValue] = useState(1);
+    //const [count, setCount] = useState(1);
+    // use memo memoizes the expensive computations
+    // with useMemo we can store result of a function call and reuse it when the dependencies of that function haven't changed,
+    //rather than recalculating the value on every render.
 
     let final_count = useMemo(function(){
         let count = 0;
@@ -16,7 +20,17 @@ export function UseMemo(){
            count = count + i;
         }
         return count;
-    },[value])
+    },[value]) // whenever value changes function is called. It is "NOT" called on every re render
+
+    /*useEffect(function(){
+        let count = 0;
+        for (let i = 1; i<= value; i++){
+            count = count + i;
+        }
+        setCount(count)
+    },[value]) */
+    // this use effect will cause unnecessary two re renders first when value updates this will
+    // cause function to run and will again update setCount which will again cause a re render
 
     return(
     <Fragment>
