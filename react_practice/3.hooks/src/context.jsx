953392f71b9a2170ef6b08cce8eb3126d8a2f9e0
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-07-18 22:10:09 +0530

    added context api

---
+++ react_practice/3.hooks/src/context.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react"
+
+export const CountContext = createContext(0);
+
+// it is the tele-porter that will teleport variables from one component to another. 
+// it will rectify the problem of prop drilling
---
+++ react_practice/3.hooks/src/context_api/Counter.jsx
@@ -0,0 +1,45 @@
+import { useContext, useState } from "react"
+import { CountContext } from "../context"
+
+export default function Counter(){
+    const [counter, setCounter] = useState(0)
+
+    // wrap anyone that wants to use the teleported value inside a .provider tag
+    // CountRenderer and Buttons are declared inside the count so they are whole Count
+    // is wrapped inside the CountContext.provider tag
+    return (
+        <div>
+            <CountContext.Provider value={counter}>
+                <Count setCounter={setCounter} />
+            </CountContext.Provider>
+        </div>
+    )
+}
+
+function Count({setCounter}) {
+    return <div>
+        <CountRenderer />
+        <Buttons setCounter={setCounter} />
+    </div>
+}
+
+function CountRenderer() {
+    const counter = useContext(CountContext)
+
+    return <div>
+        {counter}
+    </div>
+}
+
+function Buttons({setCounter}) {
+    const count = useContext(CountContext);
+    return <div>
+        <button onClick={() => {
+            setCounter(count + 1)
+        }}>Increase</button>
+
+        <button onClick={() => {
+            setCounter(count - 1)
+        }}>Decrease</button>
+    </div>
+}
