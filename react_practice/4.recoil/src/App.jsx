d33383b324a887b81c776d5373d3bc02c3162f42
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-07-19 23:33:11 +0530

    made counter with recoil

--- react_practice/4.recoil/src/App.jsx
+++ react_practice/4.recoil/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useRecoilValue,useRecoilState } from 'recoil'
 import './App.css'
+import { countAtom } from '../store/atoms/count'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function Context_Counter(){
+    return (
+        <div>
+        <Count/>
+        </div>
+    )
 }
 
-export default App
+function Count() {
+    console.log("count is re rendering")
+    return <div>
+        <CountRenderer />
+        <Buttons />
+    </div>
+}
+
+function CountRenderer() {
+
+  const count = useRecoilValue(countAtom)
+    return <div>
+      <b>
+        {count}
+      </b>
+    </div>
+}
+
+function Buttons() {
+  const [count, setCount] = useRecoilState(countAtom)
+    return <div>
+        <button onClick={() => {
+          setCount(count + 1)
+        }}>Increase</button>
+
+        <button onClick={() => {
+          setCount(count - 1)
+        }}>Decrease</button>
+    </div>
+}
