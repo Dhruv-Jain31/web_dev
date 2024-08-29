b970cf0bf78b3bd9063f62afb0b6fc97ad4df28d
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-08-29 13:14:01 +0530

    added types

---
+++ Typescript/types.ts
@@ -0,0 +1,29 @@
+// Unions: if we want to print id of user which can be number or string
+type StringOrNumber = string | number;
+
+function printId(id: StringOrNumber) {
+  console.log(`ID: ${id}`);
+}
+
+printId(101); // ID: 101
+printId("202"); // ID: 202
+
+//intersection: if we want to create a type that has property of all types/interfaces
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+interface Manager {
+    name: string;
+    department: string;
+};
+
+type TechLead = Employee & Manager;
+
+const tech_lead: TechLead = {
+    name: "Dhruv",
+    department: "Devops",
+    startDate: new Date("2024-01-01")
+}
+console.log(tech_lead);
