31a34897b646768bd4a30eaddd3dcb91f74f400f
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-11-06 17:30:40 +0530

    added enum.ts

---
+++ Typescript/src/enums.ts
@@ -0,0 +1,28 @@
+//make our code more readable and error free.
+//it is a naming system that groups related values together
+//doesn't exist in javascript
+
+import express, { Request, Response } from 'express'; // replace require with import syntax, which is more compatible with TypeScript.
+//req and res add better readability.
+const app = express();
+
+enum ResponseStatus {
+    Success = 200,
+    NotFound = 404,
+    Error = 500
+}
+
+app.get("/", (req: Request, res: Response) => {
+    if (!req.query.userId) {
+        res.status(ResponseStatus.Error).json({ message: "User ID is required." });
+        return;
+    }
+
+    // logic here
+    res.status(ResponseStatus.Success).json({ message: "Request was successful!" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
