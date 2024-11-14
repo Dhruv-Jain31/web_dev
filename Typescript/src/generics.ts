0cf6908e380d9ce3db520091f1ba6a6b228c8710
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-11-14 16:46:57 +0530

    added constraint to generic

--- Typescript/src/generics.ts
+++ Typescript/src/generics.ts
@@ -9,10 +9,7 @@ benefits:
 and TypeScript locks that type in for the rest of the function. So while the function is defined to work with "any" type,
 it’s only flexible at the point of use. After you specify the type, TypeScript enforces that type throughout the function’s execution.
 
-3.Readability: It makes the intent of the code clear. We know that a function is meant to work with different types.
-
-4. Constraints: Sometimes, you want to restrict the type that can be used with a generic. Let’s say you’re building a function
-that only works with types that have a length property (like strings or arrays). You can use constraints to enforce this.*/
+3.Readability: It makes the intent of the code clear. We know that a function is meant to work with different types.*/
 
 function identity<T>(arg: T) : T{
     return arg;
@@ -52,3 +49,15 @@ let a5 = getFirstElement<teacher>([
     {name:"Ravi",id:3,present:true}
 ]);
 console.log(a5);
+
+/*4. Constraints: Sometimes, you want to restrict the type that can be used with a generic. Let’s say you’re building a function
+that only works with types that have a length property (like strings or arrays). You can use constraints to enforce this.*/
+
+function logLength<T extends { length: number }>(item: T): void {
+    console.log(item.length);
+}
+
+logLength("Hello"); // Works: string has a length
+logLength([1, 2, 3]); // Works: array has a length
+//logLength(123); // ❌ Error: number doesn't have a length
+
