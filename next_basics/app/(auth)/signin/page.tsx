0b64ddbcf1bcff2ffa531bc161b5603414098725
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-03-15 00:29:54 +0530

    added next js basics

---
+++ next_basics/app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+"use client"; // Add this at the top
+import { SigninComponent } from "@/app/components/Signin";
+
+export default function Signin() {
+    return (
+        <SigninComponent />
+    );
+}
--- next_basics/app/components/Signin.tsx
+++ next_basics/app/components/Signin.tsx
@@ -1,6 +1,12 @@
-"use client"; // Add this at the top
+"use client"
+import { useState } from "react";
 
-export default function Signin() {
+export function SigninComponent() {
+    const [message, setMessage] = useState<string | null>(null);
+
+    function handler() {
+        setMessage("Button clicked successfully");
+    }
     return (
         <div className="h-screen flex justify-center flex-col">
             <div className="flex justify-center">
@@ -13,13 +19,17 @@ export default function Signin() {
                         <div className="pt-2">
                             <LabelledInput label="Username" placeholder="Dhruv@gmail.com" />
                             <LabelledInput label="Password" type="password" placeholder="123456" />
-                            <button
+                            <button onClick={handler}
                                 type="button"
                                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2"
                             >
                                 Sign in
                             </button>
                         </div>
+                        {/* Show message when button is clicked */}
+                        {message && (
+                            <div className="mt-4 text-green-500 text-center font-bold">{message}</div>
+                        )}
                     </div>
                 </div>
             </div>
