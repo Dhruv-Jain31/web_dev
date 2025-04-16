3b0270041728270fc0d7b199efffe25251ea6008
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-04-16 12:44:42 +0530

    updated rote.ts a bit

--- next_backend/basics/app/api/user/route.ts
+++ next_backend/basics/app/api/user/route.ts
@@ -9,6 +9,44 @@ export function GET() {
     })
 }
 
+
+export async function POST(req:NextResponse){
+    //parse body
+    try{
+        const body = await req.json();
+    const { username, passoword } = body;
+
+    //extracting headers
+    const authHeader = req.headers.get("authorization");
+    console.log("Authorization Header:", authHeader);
+
+    //extracting query params
+    const { searchParams} = new URL(req.url);
+    const nameParam = searchParams.get("name");
+    console.log("Query param (name):", nameParam);
+
+    //4 db logic
+    //await db.users.insert({username, password});
+
+    //5. send response
+    return NextResponse.json({
+        message: "You are signed up",
+        username,
+        queryName: nameParam,
+    })
+    }
+    catch(err){
+        console.error("POST ERROR:", err);
+        return NextResponse.json({
+            error : "Internal server error"
+        },
+        {
+            status: 500
+        }
+        );
+    }
+}
+
 /** app.get("/api/user",(req,res) => {
  * res.json({
  * email: "",
