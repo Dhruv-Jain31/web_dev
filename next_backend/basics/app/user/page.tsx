1878915b3133ab22a0ce651043d6b8c3a61bb1de
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-04-16 12:23:50 +0530

    added api endpoint

---
+++ next_backend/basics/app/user/page.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+async function fetchData(){
+    const response = await axios.get("http://localhost:3000/api/user")
+         await new Promise(r => setTimeout(r, 5000));
+
+         console.log("response is " + JSON.stringify(response.data));
+         return response.data;
+}
+
+export default async function User() {
+    const data = await fetchData();
+
+    return (
+        <div>
+            <div>
+            {data.email}
+            </div>
+            {data.name}
+        </div>
+    );
+}
