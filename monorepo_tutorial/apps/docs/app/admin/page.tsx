e9cec9b78e7a01edf445646351d18e39ad5e62fc
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-04-22 17:48:01 +0530

    added react project and added neccessary imports

---
+++ monorepo_tutorial/apps/docs/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { Admin } from "@repo/ui/admin";
+import { InputBox } from "@repo/ui/input-box";
+
+export default function admin (){
+    return <div>
+        hi from docs admin page
+        <Admin />
+        <InputBox />
+    </div>
+}
