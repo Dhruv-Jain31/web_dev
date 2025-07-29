582fe3cd546d79bd27eea7ef8bbbfd4b427e158b
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2025-07-29 20:06:04 +0530

    added websockets

---
+++ websocket/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { WebSocketServer } from 'ws'
+
+const app = express()
+const httpServer = app.listen(8000)
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on('connection', function connection(socket) {
+  socket.on('error', console.error);
+
+  socket.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  socket.send('Hello! Message From Server!!');
+});
